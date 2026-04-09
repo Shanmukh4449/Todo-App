58604630dd2decd290b6be660afbc628f3d44090
Shanmukh4449 <siddhartha.23bce9388@vitapstudent.ac.in>
2026-04-09 21:44:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -137,3 +137,5 @@ app.post("/edit/:index", (req, res) => {
 });
 
 app.listen(3000, () => console.log("Running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
